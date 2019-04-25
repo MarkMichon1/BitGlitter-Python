82d74824db54ab912d0e1911adc9efe9a8d0df60
Mark <markmichon777@gmail.com>
2019-04-25 17:33:53 -0500

    initial code commit

---
+++ __init__.py
@@ -0,0 +1,50 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Mark Michon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Official project page:
+# https://github.com/MarkMichon1/BitGlitter
+#
+# Guides and roadmap:
+# https://github.com/MarkMichon1/BitGlitter/wiki
+#
+# Discord channel:
+# TBA
+#
+# Have fun! :)
+
+
+# BASIC USES
+from bitglitter.read.read import read
+from bitglitter.write.write import write
+
+
+# CUSTOM PALETTE
+from bitglitter.palettes.palettefunctions import addCustomPalette, editNicknameToCustomPalette, clearAllCustomPalettes,\
+    clearCustomPaletteNicknames, printFullPaletteList, removeCustomPalette, removeCustomPaletteNickname
+
+
+# GENERAL CONFIGURATION
+from bitglitter.config.configfunctions import clearSession, clearStats, outputStats
+
+
+# MISCELLANEOUS UTILITIES
+from bitglitter.protocols.protocol_one.write.protocol_one_verifywriteparameters import paletteVerify
