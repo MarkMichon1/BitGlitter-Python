05380e5ded872bba5df5c98f31e8e9e75a13c147
Lorran Rosa <lorran@sixcreative.com.br>
2019-07-16 23:09:11 -0300

    more compat with snake case

--- bitglitter/cli/analyzer.py
+++ bitglitter/cli/analyzer.py
@@ -22,7 +22,7 @@ class CliArgumentParser(object):
             output_path = False
         
         if path.isfile(file) == True or path.isdir(file) == True:
-            read(fileToInput=file)
+            read(file_to_input=file)
         else:
             raise FileNotFoundError('Read first argument must be a valid path to a file or folder.')
 
