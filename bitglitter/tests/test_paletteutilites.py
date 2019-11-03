511f9f0215812a4215197346ad00dd689e5d976d
dgnsrekt <dgnsrekt@pm.me>
2019-11-03 15:11:49 -0600

    renamed test function

--- bitglitter/tests/test_paletteutilites.py
+++ bitglitter/tests/test_paletteutilites.py
@@ -4,7 +4,7 @@ import pytest
 from bitglitter.palettes.paletteutilities import color_distance
 
 
-def test_default_1bit():
+def test_default_1bit_palette():
     t_palette = ((0,0,0), (255,255,255))
     t_expect = 441.67
     t_result = color_distance(t_palette)
