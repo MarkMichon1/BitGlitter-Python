5865e8b48f3dc4c5eeda1bef024e265e8a5030a4
dgnsrekt <dgnsrekt@pm.me>
2019-11-03 15:10:06 -0600

    added initial paletteutilities test.

---
+++ bitglitter/tests/test_paletteutilites.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+from bitglitter.palettes.paletteutilities import color_distance
+
+
+def test_default_1bit():
+    t_palette = ((0,0,0), (255,255,255))
+    t_expect = 441.67
+    t_result = color_distance(t_palette)
+    assert t_expect == t_result
+
+
+
